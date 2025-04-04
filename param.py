82c8a296af84759a4ecf2568b64e51e072be7232
unknown <nakshathradeeva@gmail.com>
2025-04-04 15:41:57 +0530

    param file uploded

---
+++ param.py
@@ -0,0 +1,11 @@
+#Input with paramters
+
+import sys 
+
+print(" Jenkins Build Using Parameters ! ")
+
+X_VALUE = int(sys.argv[1])
+
+Y_VALUE = int(sys.argv[2])
+
+print(" Multiply = ", (X_VALUE * Y_VALUE))
